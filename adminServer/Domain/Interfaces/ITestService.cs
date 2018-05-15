575a41b44b427734963096d3e579216886910dd0
Paulus <kototensey@gmail.com>
2018-05-15 15:46:13 +0300

    started realize service/repository design pattern and created model for user

---
+++ adminServer/Domain/Interfaces/ITestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace adminServer.Domain.Interfaces
+{
+    interface ITestService<TModel> : IDisposable where TModel : class
+    {
+        IList<TModel> GetAll();
+        TModel Get(int id);
+        void Save(TModel model);
+        void Delete(int id);
+    }
+}
